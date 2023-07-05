4591618e1cae500f8931df7840f58d958af55d62
Erik Peterson <e@eriklp.com>
2023-07-04 19:00:34 -0700

    Improve tool selection process

--- autopack/selection.py
+++ autopack/selection.py
@@ -1,20 +1,44 @@
 import json
 
 from langchain.chat_models.base import BaseChatModel
-from langchain.schema import HumanMessage
+from langchain.schema import HumanMessage, SystemMessage
 
 from autopack.api import pack_search
 
-TOOL_SELECTION_PROMPT = """
-You are an autonomous AI Agent. I have a list of tools to choose from and a task I need you to accomplish. Recommend
-tools that would be required for you to complete the task. Return only a comma-separated list of tool_ids and no
-other content.
+TOOL_SELECTION_PROMPT_TEMPLATE = """You are an autonomous AI Assistant named AutoPack.
+
+AutoPack is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, AutoPack is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+AutoPack is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, AutoPack is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, AutoPack is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, AutoPack is here to assist.
+
+AutoPack has access to the following tools:
+--- TOOLS ---
+{tools_string}
+--- END TOOLS ---
+
+Respond with the following JSON structure:
+--- FORMAT INSTRUCTIONS ---
+```json
+{{"tools": [{{ "tool_id": tool_id, "reason": reason_for_recommending }}] }}
+```
+
+For example, if you think tool #1234 might be necessary:
+
+```json
+{{'tools': [{{ "tool_id": 1234, "reason": "The breathe tool might be necessary because the user needs oxygen"}}]}}
+```
+--- END FORMAT INSTRUCTIONS ---
+
+I am a human user who needs you to accomplish a task.
+First, I need you to narrow down the available tools to the ones that may be necessary to complete the following task.
 
 ---- TASK ----
 {user_input}
----- TOOLS ----
-{tools_string}
-"""
+--- END TASK ---
+
+Begin!"""
 
 
 def select_packs(task_description: str, llm: BaseChatModel) -> list[str]:
@@ -35,31 +59,39 @@ def select_packs(task_description: str, llm: BaseChatModel) -> list[str]:
     """
 
     packs = pack_search("")
-    pack_summaries = []
-    for pack in packs:
-        pack_summaries.append(
-            {
-                "tool_id": pack.pack_id,
-                "name": pack.name,
-                "description": pack.description,
-                "arguments": pack.run_args,
-            }
-        )
+    pack_summaries = ""
+    for i, pack in enumerate(packs):
+        # Use a number instead of a tool ID because the AI can mismatch tool names and repos
+        pseudo_id = i + 1
+        pack_summaries += f"""--- START TOOL #{pseudo_id} ---
+tool_id: {pseudo_id}
+name: "{pack.name}"
+description: "{pack.description}"
+arguments: "{pack.run_args}"
+--- END TOOL #{pseudo_id} ---
+"""
 
-    tools_string = json.dumps(pack_summaries)
-    prompt = TOOL_SELECTION_PROMPT.format(
-        user_input=task_description, tools_string=tools_string
+    prompt = TOOL_SELECTION_PROMPT_TEMPLATE.format(
+        user_input=task_description, tools_string=pack_summaries
     )
 
-    selected_packs = ask_llm(prompt, llm)
+    response = ask_llm(prompt, llm)
+
+    tools = json.loads(response.content)
+    # TODO Handle json errors, perhaps a retry
+
+    selected_packs = []
+    for selected_tool in tools.get("tools"):
+        tool_index = selected_tool.get("tool_id")
+        selected_packs.append(packs[tool_index - 1].pack_id)
 
     return selected_packs
 
 
 def ask_llm(prompt: str, llm: BaseChatModel) -> list[str]:
     """Encapsulate the OpenAI specific stuff to easier support other frameworks in the future"""
-    message = HumanMessage(content=prompt)
+    message = SystemMessage(content=prompt)
 
     response = llm(messages=[message])
 
-    return response.content.split(",")
+    return response
